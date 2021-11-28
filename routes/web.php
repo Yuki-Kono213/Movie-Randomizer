1bfc8100bca3cab1bef313d2c17cc3873e67afdc
Yuki Kono <hearthidebu@hotmail.co.jp>
2021-11-29 08:47:51 +0900

    ログイン機能追加

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [App\Http\Controllers\APIController::class, 'index']);
 
 Route::post('/', [App\Http\Controllers\APIController::class, 'index']);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
